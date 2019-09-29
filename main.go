8439e98fcccbc22a318d88e46310b687bdbc824a
Kaitlin Hipkin <kah5368@rit.edu>
2019-09-29 13:03:49 -0700

    use datastore to ensure no duplicated effort; also new creds

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
     "strconv"
     "strings"
 
+    "cloud.google.com/go/datastore"
+
     "github.com/jzelinskie/geddit"
 
     "google.golang.org/api/option"
@@ -21,7 +23,9 @@ const (
     redditUsername =     "CrossStitchBot"
     redditPassword =     "SECRET" // SECRET PASSWORD. DO NOT SHARE PUBLICLY.
 
+    googleCloudProjectId =     "crossstitch-bot-1569769426365"
     googleCompetitionSheetId = "1sU7OwYp9kjF0vD1uXIactca6Z_RuD6AYh7g6O5v_CPM"
+    googleCredentialsFile =    "crossstitch-bot-1569769426365-8302a8ad5d0d.json"
 )
 
 // Build the contents of the Reddit comment that will summon challenge subscribers.
@@ -34,7 +38,7 @@ func buildSummonString(ctx context.Context) (string, error) {
     // Create an authenticated Google Sheets service.
     sheetsService, err := sheets.NewService(ctx,
         option.WithScopes(sheets.SpreadsheetsReadonlyScope),
-        option.WithCredentialsFile("crossstitch-bot-1569769426365-1bf5b821811c.json"),
+        option.WithCredentialsFile(googleCredentialsFile),
     )
     if err != nil {
         log.Printf("Failed to create Google Sheets service: %v", err)
@@ -77,9 +81,32 @@ func buildSummonString(ctx context.Context) (string, error) {
 // Summons contestants to a Reddit competition post.
 func summonContestants(session *geddit.OAuthSession, post *geddit.Submission) error {
     log.Printf("Summoning contestants to post %s!", post.Permalink)
+    ctx := context.Background()
+
+    // Create an authenticated Google Cloud Datastore client.
+    dsClient, err := datastore.NewClient(ctx,
+        googleCloudProjectId,
+        option.WithCredentialsFile(googleCredentialsFile),
+    )
+    if err != nil {
+        log.Printf("Failed to create a new Datastore client: %v", err)
+        return err
+    }
+
+    // Check if this post has already been handled. If so, we're done!
+    postKey := datastore.NameKey("Entity", post.ID, nil)
+    e := struct{}{}
+    if err := dsClient.Get(ctx, postKey, &e); err != datastore.ErrNoSuchEntity{
+        if err == nil {
+            log.Print("Post has already been processed!")
+            return nil
+        }
+        log.Printf("Error checking for existence of Datastore entity for post: %v", err)
+        return err
+    }
+    log.Print("Post has not been processed yet!")
 
     // Build the summon string from Google Sheets data.
-    ctx := context.Background()
     text, err := buildSummonString(ctx)
     if err != nil {
         return err
@@ -88,7 +115,16 @@ func summonContestants(session *geddit.OAuthSession, post *geddit.Submission) er
     // Comment on the competition post to summon the subscribed users.
     log.Printf(text)
     _, err = session.Reply(post, text)
-    return err
+    if err != nil {
+        return err
+    }
+
+    // Record handling of this post.
+    if _, err := dsClient.Put(ctx, postKey, &e); err != nil {
+        log.Printf("Failed to create Datastore entity to record handling of post: %v", err)
+        return err
+    }
+    return nil
 }
 
 func main() {
