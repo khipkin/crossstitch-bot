dfdd80223f5a158c5fed35828a6ab3eb12a82f38
Kaitlin Hipkin <kah5368@rit.edu>
2019-09-29 07:07:05 -0700

    successfully authenticate with reddit and list posts

--- main.go
+++ main.go
@@ -5,15 +5,66 @@ import (
     "log"
     "net/http"
     "os"
+    "strings"
+
+    "github.com/jzelinskie/geddit"
 )
 
+const (
+    clientId =     "Kkfhbwt2W5C0Rw"
+    clientSecret = "SECRET" // SECRET PASSWORD. DO NOT SHARE PUBLICLY.
+    username =     "CrossStitchBot"
+    password =     "SECRET" // SECRET PASSWORD. DO NOT SHARE PUBLICLY.
+)
+
+func summonContestants(post *geddit.Submission) error {
+    log.Printf("Summoning contestants to post %s!", post.Permalink)
+    return nil
+}
+
 func main() {
- http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "hello world")
- })
- port := os.Getenv("PORT")
- if port == "" {
-  port = "8080"
- }
- log.Fatal(http.ListenAndServe(":"+port, nil))
+    // Authenticate.
+    session, err := geddit.NewOAuthSession(
+        clientId,
+        clientSecret,
+        "gedditAgent v1",
+        "redirect.url",
+    )
+    if err != nil {
+        log.Fatalf("Failed to create new OAuth session: %s", err)
+    }
+    if err = session.LoginAuth(username, password); err != nil {
+        log.Fatalf("Failed to authenticate: %s", err)
+    }
+
+    // Get r/CrossStitch submissions, sorted by new.
+    submissions, err := session.SubredditSubmissions("CrossStitch", geddit.NewSubmissions, geddit.ListingOptions{
+        Limit: 10,
+    })
+    if err != nil {
+        log.Fatalf("Failed to list recent submissions: %s", err)
+    }
+
+    // Check submissions for necessary actions.
+    for _, post := range submissions {
+        // Check for monthly competition post.
+        if strings.HasPrefix(post.Title, "[MOD]") && strings.Contains(post.Title, "Monthly Competition") {
+            if err := summonContestants(post); err != nil {
+                log.Fatalf("Failed to summon contestants: %s", err)
+            }
+        }
+
+        // Add more checks here!
+    }
+
+
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "hello world")
+    })
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
